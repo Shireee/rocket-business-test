04497a192a5df4b7b506de84364d3fcf09fe696c
Shireee <kirog11az@gmail.com>
2024-02-09 16:51:45 +0300

    complete Header and Nav

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -4,7 +4,17 @@ import styles from './Header.module.css';
 const Header = () => {
     return ( 
     <header className={styles.header}>
-
+        <div className={styles.logoblock}>
+            <a href="#"><img className={styles.logoblock__logo} src="./img/logo-header.svg" alt="logo"/></a>
+            <address className={styles.address}>
+                <p className={styles.address__title}>Ростов-на-Дону</p>
+                <p className={styles.address__text}>ул. Ленина, 2Б</p>
+            </address>
+        </div>
+        <div className={styles.contacts}>
+            <a href="tel:+78630000000" className={styles.contacts__phone}>+7(863) 000 00 00</a>
+            <button className={styles.contacts__button}>Записаться на прием</button>
+        </div>
     </header>    
     );
 }
--- src/componets/Nav/Nav.tsx
+++ src/componets/Nav/Nav.tsx
@@ -4,7 +4,11 @@ import styles from './Nav.module.css';
 const Nav = () => {
     return ( 
     <nav className={styles.nav}>
-
+        <a className={styles.nav__link}>О клинике</a>
+        <a className={styles.nav__link}>Услуги</a>
+        <a className={styles.nav__link}>Специалисты</a>
+        <a className={styles.nav__link}>Цены</a>
+        <a className={styles.nav__link}>Контакты</a>
     </nav>    
     );
 }
