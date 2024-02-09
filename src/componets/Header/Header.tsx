75ea741946628d4ad456f6c90694149adc56d968
Shireee <kirog11az@gmail.com>
2024-02-09 19:27:17 +0300

    complete PopUp

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Header.module.css';
+import PopUp from "../PopUp/PopUp";
 
 const Header = () => {
+    const [isPopUp, setIsPopUp] = useState<Boolean>(false);
+
     return ( 
     <header className={styles.header}>
+        {isPopUp && <PopUp onClose={() => setIsPopUp(false)} />}
         <div className={styles.logoblock}>
             <a href="#"><img className={styles.logoblock__logo} src="./img/logo-header.svg" alt="logo"/></a>
             <address className={styles.address}>
@@ -13,7 +17,7 @@ const Header = () => {
         </div>
         <div className={styles.contacts}>
             <a href="tel:+78630000000" className={styles.contacts__phone}>+7(863) 000 00 00</a>
-            <button className={styles.contacts__button}>Записаться на прием</button>
+            <button className={styles.contacts__button} onClick={ () => setIsPopUp(!isPopUp) }>Записаться на прием</button>
         </div>
     </header>    
     );
--- src/componets/PopUp/PopUp.tsx
+++ src/componets/PopUp/PopUp.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import styles from './PopUp.module.css';
 
-const PopUp = () => {
-    return ( 
-    <div className={styles.popup}>
 
-    </div>    
+
+const PopUp: React.FC<React.PropsWithChildren<{ onClose: () => void }>> = ({ onClose }) => {
+    return ( 
+    <dialog className={styles.popup}>
+        <div className={styles.popupContainer}>
+            <div className={styles.buttonConteiner}>
+                <button type="button" className={styles.button__close} onClick={onClose}></button>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.textblock}>
+                    <h2 className={styles.title}>Запишитесь на приём онлайн</h2>
+                    <h2 className={styles.text}>Администратор свяжется с вами черезWhatsApp в течение дня и уточнит детали</h2>
+                </div>
+                <form className={styles.form} action="">
+                    <input className={styles.input} type="text" placeholder="ФИО" required/>
+                    <input className={styles.input} type="tel" placeholder="Номер телефона" required/>
+                    <input className={styles.input} type="email" placeholder="Электронная почта" required/>
+                    <button className={styles.button__submit} type="submit">Записаться</button>
+                </form>
+            </div> 
+        </div>       
+    </dialog>    
     );
 }
  
