ee77c93ca659aff0f3ac940c27af2e82fec15ff7
Shireee <kirog11az@gmail.com>
2024-02-09 17:46:18 +0300

    complete Questions

--- src/componets/Question/Question.tsx
+++ src/componets/Question/Question.tsx
@@ -4,7 +4,8 @@ import styles from './Question.module.css';
 const Question = () => {
     return ( 
     <details className={styles.question}>
-  
+        <summary className={styles.question__title}>Заголовок часто задаваемого вопроса</summary>
+        <p className={styles.question__text}>Blandit et et ac non interdum odio tristique diam vestibulum. Velit in quis consequat lacus fringilla amet elementum pharetra massa. Morbi euismod nulla cras adipiscing tristique aliquet purus adipiscing. </p>
     </details>    
     );
 }
--- src/componets/Questions/Questions.tsx
+++ src/componets/Questions/Questions.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from './Questions.module.css';
+import Question from "../Question/Question";
 
 const Questions = () => {
     return ( 
     <section className={styles.questions}>
-
+        <h2 className={styles.questions__title}>Часто задаваемые вопросы</h2>
+        <Question />
+        <Question />
+        <Question />
+        <Question />
     </section>    
     );
 }
