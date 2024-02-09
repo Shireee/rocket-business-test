b7aba18fe8b426abc99761e186368648de59255e
Shireee <kirog11az@gmail.com>
2024-02-09 18:15:11 +0300

    complete Slide and Footer

--- src/componets/Footer/Footer.tsx
+++ src/componets/Footer/Footer.tsx
@@ -4,7 +4,19 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return ( 
     <footer className={styles.footer}>
-      
+        <img className={styles.logo} src="./img/logo-footer.svg" alt="logo" />
+        <nav className={styles.nav}>
+            <a className={styles.nav__link} href="#!">О клинике</a>
+            <a className={styles.nav__link} href="#!">Услуги</a>
+            <a className={styles.nav__link} href="#!">Специалисты</a>
+            <a className={styles.nav__link} href="#!">Цены</a>
+            <a className={styles.nav__link} href="#!">Контакты</a>
+        </nav>
+        <div className={styles.social}>
+            <a className={styles.social__link} href="#!"><img className={styles.social__icon} src="./img/instagram-icon.png" alt="instagram-icon"/></a>
+            <a className={styles.social__link} href="#!"><img className={styles.social__icon} src="./img/whatsapp-footer-icon.png" alt="whatsapp-icon"/></a>
+            <a className={styles.social__link} href="#!"><img className={styles.social__icon} src="./img/telegram-icon.png" alt="telegram-icon"/></a>
+        </div>
     </footer>    
     );
 }
--- src/componets/Slide/Slide.tsx
+++ src/componets/Slide/Slide.tsx
@@ -4,7 +4,27 @@ import styles from './Slide.module.css';
 const Slide = () => {
     return ( 
     <article className={styles.slide}>
-
+        <div className={styles.textblock}> 
+            <div className={styles.title}>
+                <h3 className={styles.title__sup}>CHECK-UP</h3>
+                <h4 className={styles.title__sub}>для мужчин</h4>
+            </div>
+            <ul className={styles.list}>
+                <li className={styles.list__item}>Гормональный скрининг</li>
+                <li className={styles.list__item}>Тестостерон</li>
+                <li className={styles.list__item}>Свободный тестостерон</li>
+                <li className={styles.list__item}>Глобулин, связывающий половые гормоны</li>
+            </ul>
+            <div className={styles.sum}>
+                <p className={styles.sum__after}>Всего 2800₽</p>
+                <p className={styles.sum__before}>3500₽</p>
+            </div>
+            <div className={styles.buttons}>
+                <button className={styles.buttons__appointment}>Записаться</button>
+                <button className={styles.buttons__details}>Подробнее</button>
+            </div>
+        </div>
+        <img className={styles.slide__image} src="./img/slide.png" alt="slide-img" />
     </article>    
     );
 }
